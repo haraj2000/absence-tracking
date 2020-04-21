a5bf02b02d631e112dc37cc93ee46649c6439baf
haraj2000 <haja.senabo@mail.com>
2020-04-21 21:42:43 +0000

    creation du projet

---
+++ Absence-tracking/src/main/java/com/suivi/AbsenceTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.suivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenceTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsenceTrackingApplication.class, args);
+	}
+
+}
---
+++ Absence-tracking/src/test/java/com/suivi/AbsenceTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsenceTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
